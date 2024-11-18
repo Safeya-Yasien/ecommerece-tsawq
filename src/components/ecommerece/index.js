d65f48bf56c6270d50d63cbf1159976d3a765b28
Safeya-Yasien <ssafeya5@gmail.com>
2024-11-18 18:02:32 +0200

    add time limited products section

--- src/components/ecommerece/index.js
+++ src/components/ecommerece/index.js
@@ -1,5 +1,6 @@
 import MobileMenu from "./MobileMenu";
 import CustomTitle from "./CustomTitle";
 import ProductCard from "./ProductCard";
+import ProductsListSwiper from "@pages/ProductsList";
 
-export { MobileMenu, CustomTitle, ProductCard };
+export { MobileMenu, CustomTitle, ProductCard, ProductsListSwiper };
