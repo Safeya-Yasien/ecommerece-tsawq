fcbc56c111ec108c2bbd6dadc1bfb2334d99c7c5
Safeya-Yasien <ssafeya5@gmail.com>
2024-11-20 07:41:52 +0200

    feat: create Breadcrumb.jsx

---
+++ src/components/ecommerece/Breadcrumb.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+const Breadcrumb = () => {
+  return (
+    <div className="bg-[#F5F5F5]  h-[54px] w-full px-12 ">
+      <div className="flex items-center justify-between h-full">
+        <div className="flex gap-4">
+          <Link to="/" className="text-[#767676] font-normal text-[16px]">
+            الصفحة الرئيسية
+          </Link>
+          <span className="text-[#767676]">/</span>
+          <span className="text-custom-blue font-bold text-[16px]">
+            المنتجات
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Breadcrumb;
