f18ada7d9a2a415964935865d7a7aba65d30505d
Safeya-Yasien <ssafeya5@gmail.com>
2024-11-20 08:12:27 +0200

    create 1 file and update 1 file

--- src/components/ecommerece/ThumbsGallery.jsx
+++ src/components/ecommerece/ThumbsGallery.jsx
@@ -7,32 +7,36 @@ import "swiper/css/thumbs";
 
 import { FreeMode, Thumbs, Navigation, Pagination } from "swiper/modules";
 
+// Importing local images
+import phoneImage from "../../assets/images/apple-iphone.png";
+
 const ThumbsGallery = () => {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
   return (
-    <div className="flex flex-col gap-14">
+    <div className="flex flex-col gap-14 thumbs-gallery">
       <div className="border border-[#F6F6F6] shadow-[0px_4px_19.3px_0px_#0000000D] rounded-2xl p-12">
         <Swiper
           spaceBetween={10}
           navigation={true}
-          pagination={{ clickable: true }}
+          pagination={{ clickable: true, }}
           thumbs={thumbsSwiper ? { swiper: thumbsSwiper } : undefined}
           modules={[FreeMode, Navigation, Pagination, Thumbs]}
           className="mySwiper2"
           dir="rtl"
         >
-          <SwiperSlide>
-            <img src="src/assets/images/phone.png" />
+          {/* Using local image for SwiperSlide */}
+          <SwiperSlide className="">
+            <img src={phoneImage} alt="Phone Image 1" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="src/assets/images/phone.png" />
+            <img src={phoneImage} alt="Phone Image 2" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="src/assets/images/phone.png" />
+            <img src={phoneImage} alt="Phone Image 3" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="src/assets/images/phone.png" />
+            <img src={phoneImage} alt="Phone Image 4" />
           </SwiperSlide>
         </Swiper>
       </div>
@@ -48,17 +52,21 @@ const ThumbsGallery = () => {
           dir="rtl"
           className="mySwiper cursor-pointer"
         >
-          <SwiperSlide>
-            <img src="src/assets/images/phone.png" />
+          <SwiperSlide className="border border-[#F6F6F6] rounded-lg p-6  ">
+            <img
+              src={phoneImage}
+              alt="Phone Thumbnail 1"
+              className="object-cover"
+            />
           </SwiperSlide>
-          <SwiperSlide>
-            <img src="src/assets/images/phone.png" />
+          <SwiperSlide className="border border-[#F6F6F6] rounded-lg p-6  ">
+            <img src={phoneImage} alt="Phone Thumbnail 2" />
           </SwiperSlide>
-          <SwiperSlide>
-            <img src="src/assets/images/phone.png" />
+          <SwiperSlide className="border border-[#F6F6F6] rounded-lg p-6  ">
+            <img src={phoneImage} alt="Phone Thumbnail 3" />
           </SwiperSlide>
-          <SwiperSlide>
-            <img src="src/assets/images/phone.png" />
+          <SwiperSlide className="border border-[#F6F6F6] rounded-lg p-6  ">
+            <img src={phoneImage} alt="Phone Thumbnail 4" />
           </SwiperSlide>
         </Swiper>
       </div>
