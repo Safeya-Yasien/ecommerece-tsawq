13010174d3ea164e40cd1fcc241fa59b1de31af4
Safeya-Yasien <ssafeya5@gmail.com>
2024-11-21 00:56:01 +0200

    feat: :sparkles: add category to products data

--- src/data/AllProducts.jsx
+++ src/data/AllProducts.jsx
@@ -1,4 +1,5 @@
 import phoneSection from "../assets/images/phone-section.png";
+import product from "../assets/images/phone.png";
 
 export const phonesList = [
   {
@@ -8,6 +9,7 @@ export const phonesList = [
     price: 6500,
     originalPrice: 7000,
     rating: 4.2,
+    category: "هواتف",
   },
   {
     id: 2,
@@ -16,6 +18,7 @@ export const phonesList = [
     price: 12000,
     originalPrice: 13000,
     rating: 4.8,
+    category: "هواتف",
   },
   {
     id: 3,
@@ -24,6 +27,7 @@ export const phonesList = [
     price: 9800,
     originalPrice: 10500,
     rating: 4.5,
+    category: "هواتف",
   },
   {
     id: 4,
@@ -32,6 +36,7 @@ export const phonesList = [
     price: 7400,
     originalPrice: 7800,
     rating: 4.3,
+    category: "هواتف",
   },
   {
     id: 5,
@@ -40,6 +45,7 @@ export const phonesList = [
     price: 5500,
     originalPrice: 6000,
     rating: 4.1,
+    category: "هواتف",
   },
   {
     id: 6,
@@ -48,5 +54,63 @@ export const phonesList = [
     price: 6200,
     originalPrice: 6600,
     rating: 4.4,
+    category: "هواتف",
+  },
+];
+
+export const productsList = [
+  {
+    id: 1,
+    imgSrc: product,
+    name: "سامسونج الترا جالاكسي. شريحتان اتصال بلون بنفسجي.",
+    price: 6500,
+    originalPrice: 7000,
+    rating: 4.2,
+    category: "هواتف",
+  },
+  {
+    id: 2,
+    imgSrc: product,
+    name: "آيفون 13 برو ماكس 256 جيجا",
+    price: 12000,
+    originalPrice: 13000,
+    rating: 4.8,
+    category: "لابتوب",
+  },
+  {
+    id: 3,
+    imgSrc: product,
+    name: "هواوي P40 برو 128 جيجا",
+    price: 9800,
+    originalPrice: 10500,
+    rating: 4.5,
+    category: "سماعات",
+  },
+  {
+    id: 4,
+    imgSrc: product,
+    name: "شاومي مي 11 128 جيجا",
+    price: 7400,
+    originalPrice: 7800,
+    rating: 4.3,
+    category: "لابتوب",
+  },
+  {
+    id: 5,
+    imgSrc: product,
+    name: "أوبو رينو 5 128 جيجا",
+    price: 5500,
+    originalPrice: 6000,
+    rating: 4.1,
+    category: "هواتف",
+  },
+  {
+    id: 6,
+    imgSrc: product,
+    name: "ريلمي 8 برو 128 جيجا",
+    price: 6200,
+    originalPrice: 6600,
+    rating: 4.4,
+    category: "هواتف",
   },
 ];
