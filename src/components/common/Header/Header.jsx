b6d396640b8227af96e350699e9c7c4b57ead0a8
Safeya-Yasien <ssafeya5@gmail.com>
2024-11-22 09:16:45 +0200

    feat: :sparkles: active add to cart icon button in header

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,22 +1,10 @@
-import { CiSearch, CiShoppingCart } from "react-icons/ci";
+import { CiSearch } from "react-icons/ci";
 import { MobileMenu } from "@components/ecommerece";
 import { Logo } from "..";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
 import { useMemo } from "react";
 import NavLinkItem from "./NavLinkItem";
+import ShoppingCart from "./ShoppingCart";
 
 const Header = () => {
   const navLinks = useMemo(
@@ -55,43 +43,7 @@ const Header = () => {
           <CiSearch size={24} />
         </button>
 
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button className="text-[#5F6377] bg-transparent hover:bg-transparent text-2xl ">
-              {" "}
-              <CiShoppingCart size={24} className="!w-6 !h-6" />
-            </Button>
-          </SheetTrigger>
-          <SheetContent>
-            <SheetHeader>
-              <SheetTitle>
-                السلة <span>(1)</span>
-              </SheetTitle>
-              <SheetDescription>
-                Make changes to your profile here. Click save when you're done.
-              </SheetDescription>
-            </SheetHeader>
-            <div className="grid gap-4 py-4">
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="name" className="text-right">
-                  Name
-                </Label>
-                <Input id="name" value="Pedro Duarte" className="col-span-3" />
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="username" className="text-right">
-                  Username
-                </Label>
-                <Input id="username" value="@peduarte" className="col-span-3" />
-              </div>
-            </div>
-            <SheetFooter>
-              <SheetClose asChild>
-                <Button type="submit">Save changes</Button>
-              </SheetClose>
-            </SheetFooter>
-          </SheetContent>
-        </Sheet>
+        <ShoppingCart />
       </div>
     </header>
   );
