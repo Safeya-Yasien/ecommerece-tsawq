5b9b2aaacbd2adb57858063e47c59e169e31c061
Safeya-Yasien <ssafeya5@gmail.com>
2024-11-24 02:50:53 +0200

    update PaymentFailed content

--- src/pages/PaymentFailed.jsx
+++ src/pages/PaymentFailed.jsx
@@ -13,29 +13,23 @@ const PaymentFailed = () => {
             className="object-cover"
           />
         </div>
-        <h1 className="text-[40px]">
-          <span className="font-bold text-[#263238]">شكراً لك!</span> <br />
-          <span className="text-custom-blue font-medium ">
-            لقد تم تأكيد طلبك بنجاح!
-          </span>
-        </h1>
+        <h1 className="text-[38px] font-bold ">لم تتم عملية الدفع بنجاح!</h1>
         <p className="font-medium text-lg text-[#8B939F]">
-          لقد قمنا باستلام طلبك وسيتم العمل عليه. سنتواصل معك في اقرب وقت
-          لاستلام طلبك.
+          لقد فشلت عملية الدفع, يرجي المحاولة مرة اخري.
         </p>
       </div>
 
-      <div className="flex items-center gap-4">
+      <div className="flex items-center justify-center gap-4 ">
         <Link
           to="/checkout"
-          className="bg-custom-blue text-white flex items-center justify-center flex-nowrap font-bold text-sm md:text-xl rounded-[60px] py-6 px-6 md:px-12 w-fit mx-auto"
+          className="bg-custom-blue text-white flex items-center justify-center flex-nowrap font-bold text-sm md:text-xl rounded-[60px] py-6 px-6 md:px-12 w-fit "
         >
           العودة الي الدفع{" "}
         </Link>
 
         <Link
           to="/"
-          className="bg-[#E8E8E8] text-custom-blue flex items-center justify-center flex-nowrap font-bold text-sm md:text-xl rounded-[60px] py-6 px-6 md:px-12 w-fit mx-auto"
+          className="bg-[#E8E8E8] text-custom-blue flex items-center justify-center flex-nowrap font-bold text-sm md:text-xl rounded-[60px] py-6 px-6 md:px-12 w-fit "
         >
           العودة الي الصفحة الرئيسية
         </Link>
