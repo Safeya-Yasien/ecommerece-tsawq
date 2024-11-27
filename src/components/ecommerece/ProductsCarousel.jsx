5ada2729be35c477c53a3cd6d93b7bc1a4120d31
Safeya-Yasien <ssafeya5@gmail.com>
2024-11-27 03:05:15 +0200

    update ProductsCarousel.jsx use swiper instead of carousel

--- src/components/ecommerece/ProductsCarousel.jsx
+++ src/components/ecommerece/ProductsCarousel.jsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { GoArrowLeft } from "react-icons/go";
+import { IoIosArrowRoundBack, IoIosArrowRoundForward } from "react-icons/io";
 
 import { CustomTitle, ProductCard } from "@components/ecommerece";
+import CountdownTimer from "./CountdownTimer/CountdownTimer";
 
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
-import { GoArrowLeft } from "react-icons/go";
-import CountdownTimer from "../CountdownTimer/CountdownTimer";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import { FreeMode, Navigation } from "swiper/modules";
 
 const ProductsCarousel = ({
   title,
@@ -24,33 +24,59 @@ const ProductsCarousel = ({
 
   const dateTimeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
 
+  const swiperSettings = {
+    modules: [FreeMode, Navigation],
+    navigation: {
+      prevEl: ".custom-prev",
+      nextEl: ".custom-next",
+    },
+    spaceBetween: 20,
+    slidesPerView: 2,
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 5,
+      },
+    },
+  };
+
   return (
-    <section className="flex flex-col gap-8">
-      <div className="flex items-center gap-3 flex-wrap lg:flex-nowrap">
+    <section className="flex flex-col gap-8 relative">
+      <div className="flex items-center gap-6 flex-wrap lg:flex-nowrap">
         <CustomTitle title={title} />
-        {/* countdown */}
         {showCountdown && (
           <CountdownTimer targetDate={dateTimeAfterThreeDays} />
         )}
       </div>
 
-      <Carousel className="w-full relative">
-        <CarouselContent className="-ml-1 gap-[30px]">
-          {products.map((product, index) => (
-            <CarouselItem
-              key={product.id || index}
-              className="pl-1 sm:basis-1/2 lg:basis-1/3 xl:basis-1/5"
-            >
-              <div className="p-1 ">
-                <ProductCard product={product} showLabel={showLabel} />
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
+      <Swiper {...swiperSettings} className="w-full h-full !py-4 !pr-1">
+        {products.map((product) => (
+          <SwiperSlide key={product.id}>
+            <ProductCard
+              product={product}
+              showLabel={showLabel}
+              key={product.id}
+            />{" "}
+          </SwiperSlide>
+        ))}
         {showArrows ? (
           <>
-            <CarouselPrevious className="cursor-pointer absolute left-0 -top-[53px] transform -translate-y-1/2 disabled:pointer-events-auto" />
-            <CarouselNext className="cursor-pointer absolute left-0 -top-[53px] transform -translate-y-1/2 ml-10 right-auto disabled:pointer-events-auto" />
+            <div
+              className="custom-prev absolute left-6 top-1/2 transform -translate-y-1/2 z-10 cursor-pointer
+                   bg-white w-[32px] h-[32px] rounded-full shadow-[0px_4px_16.3px_0px_#0000001a]
+                     flex items-center justify-center "
+            >
+              <IoIosArrowRoundBack size={20} color="#C6C6C6" />
+            </div>
+            <div
+              className="custom-next absolute right-6 top-1/2 transform -translate-y-1/2 z-10 cursor-pointer
+                   bg-white w-[32px] h-[32px] rounded-full shadow-[0px_4px_16.3px_0px_#0000001a]
+                     flex items-center justify-center "
+            >
+              <IoIosArrowRoundForward size={20} color="#C6C6C6" />
+            </div>
           </>
         ) : (
           <Link
@@ -63,7 +89,7 @@ const ProductsCarousel = ({
             </span>
           </Link>
         )}
-      </Carousel>
+      </Swiper>
     </section>
   );
 };
