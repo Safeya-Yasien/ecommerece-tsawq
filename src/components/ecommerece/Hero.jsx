da2c6456fd22473d48e84ba7036f6196cf695854
Safeya-Yasien <ssafeya5@gmail.com>
2024-11-27 14:39:16 +0200

    update Hero.jsx fix overflow

--- src/components/ecommerece/Hero.jsx
+++ src/components/ecommerece/Hero.jsx
@@ -15,7 +15,7 @@ export function Hero() {
       pagination={{ clickable: true }}
       className="w-full h-full"
     >
-      <SwiperSlide className="relative ">
+      <SwiperSlide className="relative overflow-hidden ">
         <div className="bg-[#F8F9FF] p-6 relative z-20">
           {/* before & after */}
           <div className="hidden md:block  absolute -left-[150px] -top-[250px] border-[60px] bg-transparent border-[#EAEDFF]  w-[635px] h-[635px] rounded-full -z-10"></div>
@@ -63,7 +63,7 @@ export function Hero() {
         </div>
       </SwiperSlide>
 
-      <SwiperSlide className="relative ">
+      <SwiperSlide className="relative overflow-hidden ">
         <div className="bg-[#F8F9FF] p-6 relative z-20">
           {/* before & after */}
           <div className="hidden md:block  absolute -left-[150px] -top-[250px] border-[60px] bg-transparent border-[#EAEDFF]  w-[635px] h-[635px] rounded-full -z-10"></div>
