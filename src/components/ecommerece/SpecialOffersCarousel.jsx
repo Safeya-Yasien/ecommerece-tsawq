717318ad4ad3cb1e7416eb65a11d901d80bfc712
Safeya-Yasien <ssafeya5@gmail.com>
2024-11-27 04:34:03 +0200

    delete 1 file and create 4 files add special offers imgs update SpecialOffersCarousel imgs

--- src/components/ecommerece/SpecialOffersCarousel.jsx
+++ src/components/ecommerece/SpecialOffersCarousel.jsx
@@ -5,46 +5,40 @@ import { GoArrowUpLeft } from "react-icons/go";
 
 import { CustomTitle } from "@components/ecommerece";
 
-import specialOffer from "../../assets/images/special-offer.png";
+import headphone from "../../assets/images/special_offers/headphone.webp";
+import ipad from "../../assets/images/special_offers/ipad.webp";
+import smartScreen from "../../assets/images/special_offers/smart_screen.webp";
+import iphone from "../../assets/images/special_offers/iphone.webp";
 
 // handle responsive
 
 const SpecialOffersCarousel = () => {
-  const offers = useMemo(
-    () => [
-      {
-        id: 1,
-        name: "سماعات وايرلس",
-        imgSrc: specialOffer,
-        discount: 50,
-      },
-      {
-        id: 2,
-        name: "سماعات وايرلس",
-        imgSrc: specialOffer,
-        discount: 50,
-      },
-      {
-        id: 3,
-        name: "سماعات وايرلس",
-        imgSrc: specialOffer,
-        discount: 20,
-      },
-      {
-        id: 4,
-        name: "سماعات وايرلس",
-        imgSrc: specialOffer,
-        discount: 70,
-      },
-      {
-        id: 5,
-        name: "سماعات وايرلس",
-        imgSrc: specialOffer,
-        discount: 70,
-      },
-    ],
-    []
-  );
+  const offers = useMemo(() => [
+    {
+      id: 1,
+      name: "سماعات وايرلس",
+      imgSrc: headphone,
+      discount: 50,
+    },
+    {
+      id: 2,
+      name: "سماعات وايرلس",
+      imgSrc: ipad,
+      discount: 50,
+    },
+    {
+      id: 3,
+      name: "سماعات وايرلس",
+      imgSrc: smartScreen,
+      discount: 20,
+    },
+    {
+      id: 4,
+      name: "سماعات وايرلس",
+      imgSrc: iphone,
+      discount: 70,
+    },
+  ]);
 
   return (
     <section className="flex flex-col gap-8">
@@ -60,7 +54,7 @@ const SpecialOffersCarousel = () => {
 
 const Offer = memo(({ product }) => {
   return (
-    <div className="flex flex-col gap-2 bg-[#F6F6F6] rounded-md p-8 w-full sm:w-[50%] md:w-[33.3%] lg:w-[20%]">
+    <div className="flex flex-col gap-2 bg-[#F6F6F6] rounded-md p-8 w-full sm:w-[50%] md:w-[33.3%] lg:w-[25%]">
       {/* info */}
       <div className="flex flex-col gap-3">
         <Link
@@ -81,10 +75,10 @@ const Offer = memo(({ product }) => {
         </a>
       </div>
       {/* img */}
-      <div className="w-[200px] h-[200px]">
+      <div className="w-[200px] h-[200px] flex items-center justify-center mr-auto">
         <img
-          src={specialOffer}
-          alt="special offers"
+          src={product.imgSrc}
+          alt={product.name}
           loading="lazy"
           className="object-cover w-full h-auto"
         />
