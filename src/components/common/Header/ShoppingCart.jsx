818fbb58143b0925c19c377e9a94b37a3ce04bee
Safeya-Yasien <ssafeya5@gmail.com>
2024-11-27 07:50:55 +0200

    update ShoppingCart.jsx closing icon

--- src/components/common/Header/ShoppingCart.jsx
+++ src/components/common/Header/ShoppingCart.jsx
@@ -15,6 +15,7 @@ import {
 } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
+import { AiOutlineClose } from "react-icons/ai";
 
 const ShoppingCart = () => {
   const {
@@ -27,10 +28,11 @@ const ShoppingCart = () => {
 
   return (
     <Sheet
+      className="!w-[85%]"
       open={isCartDrawerOpen}
       onOpenChange={isCartDrawerOpen ? closeCartDrawer : openCartDrawer}
     >
-      <SheetTrigger asChild>
+      <SheetTrigger asChild className="!w-[90%]">
         <Button className="relative text-[#5F6377] bg-transparent hover:bg-transparent text-2xl hover:text-custom-blue">
           <CiShoppingCart size={24} className="!w-6 !h-6 " />
           <span
@@ -42,18 +44,25 @@ const ShoppingCart = () => {
         </Button>
       </SheetTrigger>
       <SheetContent side="left">
-        <SheetHeader className="border-b border-b-[#E5E9F1]">
+        <SheetHeader className="!flex !flex-row !justify-between items-center border-b border-b-[#E5E9F1] !text-start pb-2">
           <SheetTitle>
-            السلة{" "}
+            <span className="font-bold text-[26px]">السلة </span>{" "}
             <span className="font-normal text-[20px] text-[#393948]">
-              ( {cartItems.length})
+              ({cartItems.length})
             </span>
           </SheetTitle>
           <SheetDescription className="sr-only">
             هنا يمكنك مراجعة العناصر في سلتك وإتمام عملية الشراء.
           </SheetDescription>
+
+          <SheetClose className="!mt-0">
+            <button className="">
+              <AiOutlineClose />
+            </button>
+          </SheetClose>
         </SheetHeader>
-        <div className="grid gap-4 py-4">
+
+        <div className="grid gap-4 py-8">
           {cartItems.map((item) => (
             <CartItem
               key={item.id}
@@ -65,11 +74,15 @@ const ShoppingCart = () => {
           ))}
           <div
             className={
-              "flex items-center border-t border-t-[#E5E9F1] mt-4 pt-4 !justify-between text-[#3A4353] font-bold text-[22px]"
+              "flex items-center border-t border-t-[#E5E9F1] mt-4 pt-4 !justify-between text-[#3A4353] "
             }
           >
-            <span className="">إجمالي السلة:</span>
-            <span className="">{getCartTotal()}ج</span>
+            <span className="font-bold text-[16px] md:text-[20px]">
+              إجمالي المبلغ:
+            </span>
+            <span className="font-bold text-lg md:text-[22px]">
+              {getCartTotal()}ج
+            </span>
           </div>
         </div>
 
